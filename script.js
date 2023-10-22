ee72c174ef57abe4363335e95db712ab302c0370
lpggil1106 <61481650+lpggil1106@users.noreply.github.com>
2023-10-22 20:31:59 +0800

    黑白交叉落子實現, reset棋盤實現(尚有Bug)

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+//宣告Canvas
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
+
+//宣告isBlack, 回合顯示
+var isBlack = true;
+var roundShow = document.getElementById("Cside");
 
 //宣告黑白子漸層顏色 grad公式:
 //對於繪製中心點為(x,y)的黑/白子 colorGrid = ctx.createLinearGradient(x - 25, y - 25, x + 25, y + 25)
@@ -16,6 +21,7 @@ whiteColor.addColorStop(1, "black");
 function drawBoard(){
 
     //繪製棋盤底()  底色=白色(reset)
+    ctx.clear;
     ctx.strokeStyle = 'black';
     ctx.lineWidth = 0.5;
     ctx.fillStyle = '#ffffff';
@@ -50,17 +56,18 @@ function drawBoard(){
 
 drawBoard();
 //黑白子測試(記得刪掉)
-ctx.beginPath();
-ctx.arc(450, 450, 18, 0, 2 * Math.PI);
-ctx.fillStyle = blackColor;
-ctx.fill();
-ctx.stroke();
-ctx.beginPath();
-ctx.strokeStyle = 'black';
-ctx.arc(500, 500, 18, 0, 2 * Math.PI);
-ctx.fillStyle = whiteColor;
-ctx.fill();
-ctx.stroke();
+// ctx.beginPath();
+// ctx.arc(450, 450, 18, 0, 2 * Math.PI);
+// ctx.fillStyle = blackColor;
+// ctx.fill();
+// ctx.stroke();
+
+// ctx.beginPath();
+// ctx.strokeStyle = 'black';
+// ctx.arc(500, 500, 18, 0, 2 * Math.PI);
+// ctx.fillStyle = whiteColor;
+// ctx.fill();
+// ctx.stroke();
 //測試結束
 
 
@@ -68,19 +75,62 @@ function drawChess(){
     
 }
 
+//繪製黑子function
+function drawBlack(xGrid, yGrid){
+    var blackColor = ctx.createLinearGradient(xGrid - 25, yGrid - 25, xGrid + 25, yGrid + 25);
+    blackColor.addColorStop(0, "white");
+    blackColor.addColorStop(0.5, "black");
+
+    ctx.beginPath();
+    ctx.arc(xGrid, yGrid, 18, 0, 2 * Math.PI);
+    ctx.fillStyle = blackColor;
+    ctx.fill();
+    ctx.stroke();
+}
+
+//繪製白子function
+function drawWhite(xGrid, yGrid){
+    var whiteColor = ctx.createLinearGradient(xGrid - 25, yGrid - 25, xGrid + 25, yGrid + 25);
+    whiteColor.addColorStop(0, "white");
+    whiteColor.addColorStop(0.6, "white");
+    whiteColor.addColorStop(1, "black");
+
+    ctx.beginPath();
+    ctx.arc(xGrid, yGrid, 18, 0, 2 * Math.PI);
+    ctx.fillStyle = whiteColor;
+    ctx.fill();
+    ctx.stroke();
+}
 
 //座標測試 X座標公式 : (2*點擊X絕對位置 - 當前視窗寬度)/100 + 7
 //        Y座標公式 : (點擊Y絕對位置-100)/50
 
 c.addEventListener ('click', event => {
-    console.log(`x: ${event.clientX}, y: ${event.clientY}`)
     var clickX = event.clientX;
     var clicky = event.clientY;
     var clientWidth = window.innerWidth;
 
+    //坐標系 Ex: [0, 0],[7, 8]
     var xCoordinate = Math.round((2*clickX - clientWidth)/100 + 7);
     var yCoordinate = Math.round((clicky - 100)/50);
 
-    console.log("x: " + xCoordinate);
-    console.log("y: " + yCoordinate);
+    console.log("x:" + xCoordinate + " y:" + yCoordinate);
+
+    //Canvas Grid Ex:[450, 450],[900, 550]
+    var xGrid = (xCoordinate + 1) * 50;
+    var yGrid = (yCoordinate + 1) * 50;
+
+    //繪製黑色棋子
+    if(xCoordinate >= 0 && yCoordinate >= 0 && xCoordinate < 15 && yCoordinate < 15){
+        if(isBlack){
+            drawBlack(xGrid, yGrid);
+            isBlack = false;
+            roundShow.innerText = "白子的回合"
+
+        }else{
+            drawWhite(xGrid, yGrid);
+            isBlack = true;
+            roundShow.innerText = "黑子的回合"
+        }
+    }
   })
